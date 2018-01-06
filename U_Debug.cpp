c571fe0aa76d57fc3339f39754f3751e72951096
romeli <fengzheng1995@gmail.com>
2018-01-06 15:36:43 +0800

    修改了User Dubeg的开启条件为Debug模式

--- U_Debug.cpp
+++ U_Debug.cpp
@@ -11,7 +11,7 @@
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
 void U_Debug::Print(uint8_t* file, uint32_t line, const char* message) {
-#ifdef USER_DEBUG
+#ifdef DEBUG
 	trace_printf("U_assert() failed: file \"%s\", line %d, message \"%s\"",
 			file, line, message);
 	trace_printf("\n");
